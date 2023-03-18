865b52f9a23e6c574fa58f51fb767c09aceddd2d
Jesse Both <jesse9both@gmail.com>
2023-03-18 06:07:28 -0400

    Update word count output if the time has not expired

--- src/timer.c
+++ src/timer.c
@@ -13,9 +13,16 @@ int timerContinue(uint8_t set, uint8_t val){
   return (int)timerExit;
 }
 
+int timerDecrement(uint8_t dec) {
+  static uint8_t i = 60;
+  i -= dec;
+
+  return i;
+}
+
 void timer(){
   timerContinue(1,1);
-  for(int i = 60; i >= 0 && timerContinue(0,0); i--){
+  for(uint8_t i = timerDecrement(0); i >= 0 && timerContinue(0,0); i=timerDecrement(1)){
     printf(CURSOR_SAVE TIMER_POS "Time: %d \n" CURSOR_RESTORE, i);
     sleep(1);
   }
